abb306019d3b40c50f224bd96286ebf85b9729f3
eddym <edwinmuthee11@gmail.com>
2022-05-20 15:42:51 +0300

    use of return

--- src/functions.kt
+++ src/functions.kt
@@ -4,6 +4,11 @@ fun myFunction(fname: String, age: Int) {
 fun myFunctions(x: Int): Int {
     return (x + 5)
 }
+// A function with two Int parameters, and Int return type
+fun myFunctin(x: Int, y: Int): Int {
+    return (x + y)
+}
+
 
 fun main() {
     myFunction("John", 35)
@@ -12,4 +17,6 @@ fun main() {
 
     var result = myFunctions(3)
     println(result)
+    var results = myFunctin(3,5)
+    println(results)
 }
