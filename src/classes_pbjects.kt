cd733166dd11fb91b425ea88f24c8d686cff9c78
eddym <edwinmuthee11@gmail.com>
2022-05-20 16:03:12 +0300

    constructor creation

--- src/classes_pbjects.kt
+++ src/classes_pbjects.kt
@@ -3,10 +3,11 @@ class Car {
     var model = ""
     var year = 0
 }
+class Cars(var brand: String, var model: String, var year: Int)
 fun main(){
     // Create a c1 object of the Car class
     val c1 = Car()
-
+    val c2 = Cars("Range Rover", "Mustanga", 1969)
 // Access the properties and add some values to it
     c1.brand = "Ford"
     c1.model = "Mustang"
@@ -15,4 +16,7 @@ fun main(){
     println(c1.brand)   // Outputs Ford
     println(c1.model)   // Outputs Mustang
     println(c1.year)    // Outputs 1969
+    println(c2.brand)   // Outputs Ford
+    println(c2.model)   // Outputs Mustang
+    println(c2.year)
 }
